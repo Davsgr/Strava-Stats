d127cc087c80eae065595d96c1db69ac8240b622
David <david.graf28@gmail.com>
2020-07-29 11:56:06 +0200

    First commit with some call to the STRAVA API

---
+++ scripts/app.js
@@ -0,0 +1,60 @@
+// Get the authorization 
+const urlParams = new URLSearchParams(window.location.href);
+const code = urlParams.get('code');
+
+const clientId = '51577';
+const clientSecret = '55b529553cdd14a1a87344b4ad1c80ced3fb784b';
+const getToken = 'https://www.strava.com/api/v3/oauth/token';
+
+const urlAPI = 'https://www.strava.com/api/v3/'
+
+
+
+const getData = async () => {
+    const response = await fetch(getToken +`?client_id=${clientId}&client_secret=${clientSecret}&code=${code}&grant_type=authorization_code`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const dataAthlete = await response.json(); //extract JSON from the http response
+    return dataAthlete;
+  }
+
+if(code){
+    getData()
+        .then(athlete => { 
+            console.log('Your are logged In', athlete.athlete.username);
+            updateUI(athlete);
+        })
+        .catch( err => console.log(err));
+}
+else{
+    console.log('Your are not connected');
+}
+
+const getAthleteStats = async (id, token) => {
+    const response = await fetch(urlAPI + `/athletes/${id}/stats`, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        }
+    });
+    const athleteStats = await response.json();
+    return athleteStats;
+}
+
+// activity
+
+const getActivities = async (token) => {
+    const response = await fetch(urlAPI + `/athlete/activities?per_page=10`, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        }
+    });
+    const activities = await response.json();
+    return activities;
+}
---
+++ scripts/ui.js
@@ -0,0 +1,49 @@
+const loggin = document.querySelector('.loggin');
+const loggout = document.querySelector('.loggout');
+loggout.style.display = 'none';
+const title = document.querySelector('.title');
+const header = document.querySelector('.header');
+const lastRide = document.querySelector('.last-ride');
+
+loggin.addEventListener('click', () =>{
+    window.location.href = 'https://www.strava.com/oauth/authorize?client_id=51577&response_type=code&redirect_uri=http://localhost:5500&approval_prompt=auto&scope=profile:read_all,activity:read';
+});
+
+loggout.addEventListener('click', () =>{
+    window.location.href = '/';
+});
+
+const addImgProfil = (linkImg) => {
+    const img = document.createElement('img');
+    img.src= linkImg;
+    header.appendChild(img);
+    console.log(img);
+ }
+
+ const updateActivitesUI = (activities) => {
+     activities.forEach( activite => {
+        const html = `<li> distance: ${(activite.distance/1000).toFixed(2)} km 
+                        Sport : ${activite.type}    
+                    </li>`;
+        lastRide.innerHTML += html;
+     });
+ }
+
+const updateUI = (athlete) => {
+
+    console.log(athlete);
+    console.log('Welcome on the UI', athlete.athlete.username);
+    addImgProfil(athlete.athlete.profile);
+    loggin.style.display = 'none';
+    loggout.style.display = 'block';
+    title.innerHTML = `${athlete.athlete.firstname} ${athlete.athlete.lastname}`;
+
+    getAthleteStats(athlete.athlete.id, athlete.access_token)
+        .then( res => console.log('Stats athlete' , res))
+        .catch( err => console.log(err));
+
+    getActivities( athlete.access_token)
+        .then(activities => updateActivitesUI(activities))
+        .catch(err => console.log(err));
+}
+
