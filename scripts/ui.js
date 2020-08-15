8f895a200027d6c38ceb05c72c878495e7121a75
David <david.graf28@gmail.com>
2020-08-15 16:40:15 +0200

    Add date and finish the strcuture of the website

--- scripts/ui.js
+++ scripts/ui.js
@@ -7,6 +7,14 @@ const title = document.querySelector('.title');
 const header = document.querySelector('.header');
 const statsTitle = document.querySelector('.stats-title');
 const lastRide = document.querySelector('.last-ride');
+const error = document.querySelector('.error');
+
+const totalBike = document.querySelector('.total-bike');
+const bikeBar = document.querySelector('.bike');
+const totalRun = document.querySelector('.total-run');
+const runBar = document.querySelector('.run');
+const totalSwim = document.querySelector('.total-swim');
+const swimBar = document.querySelector('.swim');
 
 loggin.addEventListener('click', () =>{
     window.location.href = 'https://www.strava.com/oauth/authorize?client_id=51577&response_type=code&redirect_uri=http://localhost:5500&approval_prompt=auto&scope=profile:read_all,activity:read';
@@ -26,18 +34,39 @@ const addImgProfil = (linkImg) => {
  const updateActivitesUI = (activities) => {
      console.log(activities);
      stats.style.display='flex';
+     let run = 0;
+     let swim = 0;
+     let bike = 0;
      activities.forEach( activite => {
         if (activite.type === 'Run'){
-            const html = `<li>distance: ${(activite.distance/1000).toFixed(2)} km Sport : <i class="fas fa-running"></i></li>`;
+            const html = `<li>distance: &nbsp&nbsp&nbsp <span class="dist">${(activite.distance/1000).toFixed(2)} km</span>
+                            Sport : <i class="fas fa-running"></i></li> 
+                            ${dateFns.format(activite.start_date_local,'DD/MM/YYYY')}`;
             lastRide.innerHTML += html;
+            ++run;
+            runBar.style.width = run*200/10;
+            totalRun.textContent = run;
         } else if (activite.type === 'Swim'){
-            const html = `<li>distance: ${(activite.distance/1000).toFixed(2)} km Sport : <i class="fas fa-swimmer"></i></li>`;
+            const html = `<li>distance: &nbsp&nbsp&nbsp <span class="dist">${(activite.distance/1000).toFixed(2)} km</span>
+                            Sport : <i class="fas fa-swimmer"></i></li> 
+                            ${dateFns.format(activite.start_date_local,'DD/MM/YYYY')}`;
             lastRide.innerHTML += html;
-        } else if (activite.type === 'Bike'){
-            const html = `<li>distance: ${(activite.distance/1000).toFixed(2)} km Sport : <i class="fas fa-biking"></i></li>`;
+            ++swim;
+            swimBar.style.width = swim*200/10;
+            totalSwim.textContent = swim;
+        } else if (activite.type === 'Ride'){
+            const html = `<li>distance: &nbsp&nbsp&nbsp <span class="dist">${(activite.distance/1000).toFixed(2)} km</span>
+                            Sport : <i class="fas fa-biking"></i></li> 
+                            ${dateFns.format(activite.start_date_local,'DD/MM/YYYY')}`;
             lastRide.innerHTML += html;
+            ++bike;
+            bikeBar.style.width = bike*200/10;
+            totalBike.textContent = bike;
+            console.log(bike);
         } else {
-            const html = `<li>distance: ${(activite.distance/1000).toFixed(2)} km Sport : <i class="fas fa-biking"></i></li>`;
+            const html = `<li>distance: &nbsp&nbsp&nbsp <span class="dist">${(activite.distance/1000).toFixed(2)} km</span>
+                            Sport : <i class="fas fa-biking"></i></li> 
+                            ${dateFns.format(activite.start_date_local,'DD/MM/YYYY')}`;
             lastRide.innerHTML += html;
         }
         
@@ -64,7 +93,7 @@ const updateUI = (athlete, scopeEnable) => {
             .catch(err => console.log(err));
         }
     else {
-        lastRide.innerHTML = '<p>Vous devez autoriser la lecture des données de vos activités pour voir vos stats.</p>'
+        error.innerHTML = '<p>Vous devez autoriser la lecture des données de vos activités pour voir vos stats.</p>'
     }
 }
 
